5b22f4b17a47d480764a3d307cbfc82f90c50352
Abel Pastor Orts <abel.pastor@redhub.com.au>
2019-10-30 13:31:36 +1100

    Getting fedora docker ready

--- ready.sh
+++ ready.sh
@@ -25,4 +25,15 @@ then
 else
   sudo ${OS} remove -y docker docker-engine docker.io
 fi
+echo "Getting new version of docker"
 
+if [OS -eq "fedora"]
+then
+  sudo dnf -y install dnf-plugins-core
+  sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+  sudo dnf install -y docker-ce docker-ce-cli containerd.io
+
+  echo "Installing Docker Compose"
+  sudo dnf install docker-compose
